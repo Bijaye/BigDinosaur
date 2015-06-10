52736899a894e5fa37ce23ab2aec63ab2b40103c
abishkar <abishkar@abishkar-PC>
2015-06-10 00:04:11 -0700

    added refactored package structure and junit run  successfully

---
+++ BigDinosaur-Core/src/test/java/org/bigdinosaur/configuration/test/HadoopVersionAnnotationTest.java
@@ -0,0 +1,10 @@
+package org.bigdinosaur.configuration.test;
+
+import org.bigdinosaur.conf.Configuration;
+
+public class HadoopVersionAnnotationTest
+{
+  public static void main(String[] args) throws Exception {
+    new Configuration().writeXml(System.out);
+  }
+}
