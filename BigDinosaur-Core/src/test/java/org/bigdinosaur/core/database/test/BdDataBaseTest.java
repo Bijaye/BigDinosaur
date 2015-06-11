32b1804d6038e0bdd4f5a0cb253aa935c00e8a17
abishkar <abishkar@abishkar-PC>
2015-06-10 21:30:25 -0700

    added

---
+++ BigDinosaur-Core/src/test/java/org/bigdinosaur/core/database/test/BdDataBaseTest.java
@@ -0,0 +1,64 @@
+package org.bigdinosaur.core.database.test;
+
+import org.bigdinosaur.core.database.BdDataBase;
+
+public class BdDataBaseTest
+{
+  public static void main(String[] args)
+  {
+    // Input 
+    //DatabaseName:HistoryData:ObjectType:ErrorObject
+    //OutPut 
+    //List of errorObject
+    //Imp:data normalization is very important consider this .Should not have redundancy in used space and in object retrieve process
+  
+      
+    }
+  
+BdDataBase getSingleClusterDatabase(){
+  return new BdDataBase();
+  
+
+}
+BdDataBase getMultipleClusterMultipleDatabase(String pattern){
+  return new BdDataBase();
+  
+
+}
+BdDataBase getSingleClusterHistoryDatabase(String pattern){
+  return new BdDataBase();
+  
+
+}
+
+BdDataBase getSingleClusterAllDatabases(String pattern){
+  return new BdDataBase();
+  
+
+}
+BdDataBase getSingleClusterSessionDatabase(String pattern){
+  return new BdDataBase();
+  
+
+}
+BdDataBase getSingleClusterServiceDatabase(String pattern){
+  return new BdDataBase();
+  
+
+}
+BdDataBase getHistoryTreeSingleCluster(String pattern){
+  return new BdDataBase();
+}
+BdDataBase getHistoryTreeMultipleCluster(String pattern){
+  return new BdDataBase();
+}
+// 10 thousand nodes is supposed to be supported by big BigDinosaur platform
+int getMaxClusterNodes(){
+  return 0;
+  
+}
+int getMaxDataBaseOnAllCluster(){
+  return 0;
+  
+}
+}
