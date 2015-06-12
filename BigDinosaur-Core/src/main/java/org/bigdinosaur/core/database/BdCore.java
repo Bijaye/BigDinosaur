9e79834651f4aae6d03161e9fe04eabf5618406a
abishkar <abishkar@abishkar-PC>
2015-06-11 20:55:01 -0700

    Added BdCore Interface

---
+++ BigDinosaur-Core/src/main/java/org/bigdinosaur/core/database/BdCore.java
@@ -0,0 +1,16 @@
+package org.bigdinosaur.core.database;
+
+public interface BdCore
+{
+ boolean canRead(Object o);
+ boolean canWrite(Object o);
+ Object getAsBdFile(Object o);
+ Object getAsBdTableNode(Object o);
+ Object getAsBdQuery(Object o);
+ Object getAsBdFileRow(Object o);
+ // Universal Map scope is Global within the application .Any time can get value 
+ Object AddOnUniversalMap(Object key,Object value);
+ 
+ 
+
+}
