173f183ff117f4f5f8a8842e2e26d129afdc47e9
abishkar <abishkar@abishkar-PC>
2015-06-14 02:38:42 -0700

    added simple abstraction

---
+++ BigDinosaur-Core/src/main/java/org/bigdinosaur/core/database/BdSimpleAbs.java
@@ -0,0 +1,8 @@
+package org.bigdinosaur.core.database;
+
+//A table and storage management service for BigDinosaur data that presents a table
+//abstraction so the user does not need to know where or how the data is stored.
+public interface BdSimpleAbs
+{
+
+}
