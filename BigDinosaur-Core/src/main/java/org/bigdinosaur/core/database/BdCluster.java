d2973fc9f80bd28d977589740d3cad839e226d45
abishkar <abishkar@abishkar-PC>
2015-06-16 23:34:48 -0700

    added

--- BigDinosaur-Core/src/main/java/org/bigdinosaur/core/database/BdCluster.java
+++ BigDinosaur-Core/src/main/java/org/bigdinosaur/core/database/BdCluster.java
@@ -2,7 +2,19 @@ package org.bigdinosaur.core.database;
 // it is the combination of master node and slaves nodes
 public interface BdCluster
 {
+  // 
 Object  getMasterNode();
 Object [] getSlavesNode();
+Object getClusterAllocatedMemory();
+Object getClusterAvailableMemory();
+Object getClusterAllocatedCore();
+Object getClusterAvailableCore();
+Object getJvmFreeMemory();
+Object getJvmavailableMemory();
+Object getJvmTotalMemory();
+Object getNoOfClusterRunningApplicaiton();
+Object getNoOfClusterRunningContainer();
+
+
 
 }
---
+++ BigDinosaur-Core/src/main/java/org/bigdinosaur/core/database/BdClusterGroupManager.java
@@ -0,0 +1,6 @@
+package org.bigdinosaur.core.database;
+// from here node can be removed from the cluster ,added in cluster,updated in cluster,
+public interface BdClusterGroupManager
+{
+
+}
