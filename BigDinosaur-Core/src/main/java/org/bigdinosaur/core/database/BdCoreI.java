8882b13a4ca0c5aff58377b6519e3d6ee4115216
abishkar <abishkar@abishkar-PC>
2015-06-18 11:19:42 -0700

    added

---
+++ BigDinosaur-Core/src/main/java/org/bigdinosaur/core/database/BdCoreI.java
@@ -0,0 +1,102 @@
+package org.bigdinosaur.core.database;
+
+public class BdCoreI implements BdCore
+{
+private String pigeonhole;//it can be property or action 
+//for example pigeonhole=Property or pigeonhole=action;
+private String propertyname;
+//propertyname=file;
+private String displayname;
+//displayname=file;
+private String propertydescription;
+//propertydescription=static or dynami
+private String type;
+//what type of object it is 
+
+private String flags;
+//read or write or read write what it is 
+  @Override
+  public boolean canRead(Object o)
+  {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean canWrite(Object o)
+  {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Object getAsBdFile(Object o)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Object getAsBdTableNode(Object o)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Object getAsBdQuery(Object o)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Object getAsBdFileRow(Object o)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Object AddOnUniversalMap(Object key, Object value)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public BdCore getBdCore()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Boid getBoird()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public BdSpace getBdSpace()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public BdSession getBdSession()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public BdAcl getBdAcl()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
--- BigDinosaur-Core/src/main/java/org/bigdinosaur/core/database/BdType.java
+++ BigDinosaur-Core/src/main/java/org/bigdinosaur/core/database/BdType.java
@@ -3,6 +3,7 @@ package org.bigdinosaur.core.database;
 //bg framework is dealing with
 //Every system if decomposed then it can be abstracted into different no of objecttype.
 //that makes the system
+//if there are 20 objects on bd core framework then it will be easier to represent them in object type
 public class BdType
 {
 
