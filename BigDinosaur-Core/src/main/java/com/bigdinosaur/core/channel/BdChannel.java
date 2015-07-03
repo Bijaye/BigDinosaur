9f6475e052091d101750f39000be9f5e5fd527ae
abishkar <abishkar@abishkar-PC>
2015-07-03 11:23:37 -0700

    added

---
+++ BigDinosaur-Core/src/main/java/com/bigdinosaur/core/channel/BdChannel.java
@@ -0,0 +1,21 @@
+package com.bigdinosaur.core.channel;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public interface BdChannel
+{
+  public abstract Object decode(DataInput paramDataInput)
+      throws IOException;
+
+  public abstract void encode(DataOutput paramDataOutput)
+      throws IOException;
+  
+  void sendDataToClient(ServerSocket serverSocket, DataInputStream dataInputStream,
+                        DataOutputStream dataOutputStream);
+Object getObjectToSendToClient( DataInputStream dataInputStream);
+}
