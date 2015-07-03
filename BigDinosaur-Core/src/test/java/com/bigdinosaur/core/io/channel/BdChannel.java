e63c07d7695e0198234fa938f1cdb0aa714d620d
abishkar <abishkar@abishkar-PC>
2015-07-03 15:13:42 -0700

    added

--- BigDinosaur-Core/src/test/java/com/bigdinosaur/core/io/channel/BdChannel.java
+++ BigDinosaur-Core/src/test/java/com/bigdinosaur/core/io/channel/BdChannel.java
@@ -5,7 +5,10 @@ public class BdChannel
   public static void main(String[] args)
   {
     BdChanneCl channel=new BdChanneCl();
+   
     channel.connectServer("127.0.0.1");
+    //pass files 
+    
 //    channel.passObject(obj)
   }
 
