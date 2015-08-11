df6fa0621ccfc2e57d4a8d7c6af70617da607bba
abishkar <abishkar@abishkar-PC>
2015-08-11 01:57:28 -0700

    added

---
+++ BigDinosaur-Core/src/test/java/com/bd/core/abstractdatatype/BdCoreAdt.java
@@ -0,0 +1,9 @@
+package com.bd.core.abstractdatatype;
+// it defines abstract data type from the view of the user 
+// purposed e.g
+//DataAdt
+//insertContent(NodeSite,SiteDir,SiteContent)
+public class BdCoreAdt
+{
+
+}
