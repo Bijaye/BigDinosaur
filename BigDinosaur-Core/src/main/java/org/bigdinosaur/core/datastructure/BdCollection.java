4a1ac16208a93b7ab312b54b8dde929bf946128b
abishkar <abishkar@abishkar-PC>
2015-08-10 21:47:24 -0700

    added

--- BigDinosaur-Core/src/main/java/org/bigdinosaur/core/datastructure/BdCollection.java
+++ BigDinosaur-Core/src/main/java/org/bigdinosaur/core/datastructure/BdCollection.java
@@ -7,34 +7,24 @@ public interface BdCollection<E> extends BdIterable<E> {
 
     boolean contains(Object o);
    
-    BdIterator<E> iterator();
-   
-  
-    
+    BdIterator<E> iterator(); 
     boolean add(E e);
-   
     boolean remove(Object o);
-
-    
     boolean containsAll(BdCollection<?> c);
-
-   
-
-   
-
-   
     void clear();
-
-
     boolean equals(Object o);
-
-   
     int hashCode();
     boolean addAll(BdCollection<? extends E> c);
    boolean  addPriority(Object obj,String prioritylevel);
    Object Search(Object obj);
    Object checkDublicate(Object obj);
    BdCollection  sort(BdCollection coll);
+   BdCollection getInInsertionOrder(BdCollection coll);
+   BdCollection getSorted(BdCollection coll);
+   // no dublication allowed 
+   BdCollection getUniqueObjects(BdCollection coll);
+   BdCollection getSortedByKey(BdCollection coll);
+   BdCollection getSortedByKeyValueInsertionOrder(BdCollection coll);
    int countNodes(BdCollection coll);
   boolean put(Object key,Object value);
   boolean get(Object key);
