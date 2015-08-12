83b927736f5dc02d687f2f9d3a6eb308b990c560
abishkar <abishkar@abishkar-PC>
2015-08-12 01:55:11 -0700

    refactored

--- BigDinosaur-Core/src/main/java/com/bigdinosar/security/extend/OAuthJSONAccessTokenResponseExtended.java
+++
@@ -1,9 +0,0 @@
-package com.bigdinosar.security.extend;
-
-import org.apache.oltu.oauth2.client.response.OAuthJSONAccessTokenResponse;
-
-
-
-public class OAuthJSONAccessTokenResponseExtended extends OAuthJSONAccessTokenResponse {
-
-}
