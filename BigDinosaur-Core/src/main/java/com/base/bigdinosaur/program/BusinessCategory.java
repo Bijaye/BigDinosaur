081f35aad12286191c0388675055a9b38754bd5c
abishkar <abishkar@abishkar-PC>
2015-08-12 23:11:09 -0700

    removed

--- BigDinosaur-Core/src/main/java/com/base/bigdinosaur/program/BusinessCategory.java
+++
@@ -1,5 +0,0 @@
-package com.base.bigdinosaur.program;
-
-public class BusinessCategory {
-
-}
--- BigDinosaur-Core/src/main/java/com/company/service/IManagementService.java
+++
@@ -1,8 +0,0 @@
-package com.company.service;
-
-import com.company.model.BaseJmx;
-
-public interface IManagementService {
-	BaseJmx Register(BaseJmx management) throws Exception;
-
-}
