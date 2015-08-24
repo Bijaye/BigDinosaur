a386d4dc63e66c755452200bbba53b615320a149
abishkar <abishkar@abishkar-PC>
2015-08-23 22:05:44 -0700

    added

--- BigDinosaur-Core/src/com/bigdinosaur/core/fs/PositionedReadable.java
+++ BigDinosaur-Core/src/com/bigdinosaur/core/fs/PositionedReadable.java
@@ -1,10 +1,7 @@
 package com.bigdinosaur.core.fs;
 
+import java.io.IOException;
 
-import java.io.*;
-
-
-/** Stream that permits positional reading. */
 
 public interface PositionedReadable {
   /**
@@ -12,6 +9,13 @@ public interface PositionedReadable {
    * position within a file, and return the number of bytes read. This does not
    * change the current offset of a file, and is thread-safe.
    */
+  
+  //position=seek position
+  //seek=5=position
+    //  offset=2;
+  //lenght=20;
+  //read starts from position 5 ,read 10 bytes and copy to buffer array starting from position 2
+ 
   public int read(long position, byte[] buffer, int offset, int length)
     throws IOException;
   
@@ -20,6 +24,7 @@ public interface PositionedReadable {
    * position within a file. This does not
    * change the current offset of a file, and is thread-safe.
    */
+  
   public void readFully(long position, byte[] buffer, int offset, int length)
     throws IOException;
   
